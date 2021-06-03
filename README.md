8fe4f27cc1578c1b30ea74cc0cdeb3e3e0eab060
German Heinz <gerheinz@everis.com>
2021-06-04 00:22:49 +0200

    Redux - Actions - First dispatch

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ https://react-redux.js.org/
 
 https://es.redux.js.org/
 
-Instalation
+-- Instalation
 
 1 - npm install react-redux redux
 
@@ -19,3 +19,34 @@ Instalation
 4 - Add Provider at the entire app in JournalApp
 
 4 - Configuring Redux DevTools -> https://github.com/zalmoxisus/redux-devtools-extension#usage
+
+
+-- Custom Hook useForm
+
+<!-- import { useState } from 'react';
+
+
+export const useForm = ( initialState = {} ) => {
+    
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues( initialState );
+    }
+
+
+    const handleInputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        });
+
+    }
+
+    return [ values, handleInputChange, reset ];
+
+} -->
+
+
+5 - Creating the action
--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useForm } from '../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/authAction';
+
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [ values, handleInputChange ] = useForm({
+        email: 'german@gmail.com',
+        password: '123456'
+    });
+
+    const { email, password } = values;
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        dispatch( login(123457, 'Germám') )
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
+            <form onSubmit={ handleOnSubmit }>
 
                 <input 
                     type="text"
@@ -14,6 +33,8 @@ export const LoginScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={ email }
+                    onChange={ handleInputChange }
                 />
 
                 <input 
@@ -21,6 +42,8 @@ export const LoginScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value={ password }
+                    onChange={ handleInputChange }
                 />
 
 
